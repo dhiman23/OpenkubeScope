020efd0eb8a4a35471bf2c479aebede5421036e5
Dhiman23 <sajaldhiman68@gmail.com>
2026-07-19 01:51:50 +0300

    added

--- git-governance/branch-protection.tf
+++ git-governance/branch-protection.tf
@@ -16,7 +16,7 @@ resource "github_repository_ruleset" "openkubescope_main_ruleset" {
 
   rules {
     creation                = false
-    update                  = true
+
     deletion                = false
     required_linear_history = true
     required_signatures     = false
